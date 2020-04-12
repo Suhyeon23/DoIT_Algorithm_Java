cc52ca3d7e458d7ac23e2904b404a66001975e74
Suhyeon23 <38066418+Suhyeon23@users.noreply.github.com>
2020-04-13 01:29:24 +0900

    피라미드

---
+++ 1장 기본 알고리즘/chap01/day1_16.java
@@ -0,0 +1,18 @@
+class day1_16 {
+//n단의 피라미드 메서드 (i행에는 (i-1)*2+1개의 * , 마지막 n행에는 (n-1)*2+1개의 *
+static void spira(int n) {
+
+	for(int i=1;i<=n;i++) {
+		for(int j=n;j>=i;j--) {
+			System.out.print(" ");
+		}
+		for(int k=1;k<=(i-1)*2+1;k++){
+			System.out.print("*");
+			}
+			System.out.println(" ");
+	}
+}
+	public static void main(String[] args) {
+		spira(4);
+	}
+}
